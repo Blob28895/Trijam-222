2c5e21a1ba6901e7cb6b49656a8ed045ecad9d76
Collin Newman <collinnewman12@gmail.com>
2023-06-04 05:04:50 -0500

    add half working code

--- Assets/Scripts/PaintingController.cs
+++ Assets/Scripts/PaintingController.cs
@@ -14,16 +14,92 @@ public class PaintingController : MonoBehaviour, IPointerDownHandler, IDragHandl
     private RawImage rawImage;
     private Vector2 previousPosition;
 
+    private int textureWidth = 800;
+    private int textureHeight = 800;
+
     private void Start()
     {
         rawImage = GetComponent<RawImage>();
         canvasRect = rawImage.rectTransform;
-        canvasTexture = new Texture2D(800, 800);
+        canvasTexture = new Texture2D(textureWidth, textureHeight);
         rawImage.texture = canvasTexture;
 
         mainCamera = Camera.main;
 
         Cursor.SetCursor(mouseCursor, Vector2.zero, CursorMode.Auto);
+
+        DrawObstaclesOnTexture();
+    }
+
+    private void DrawObstaclesOnTexture()
+    {
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+
+        foreach (GameObject obstacle in obstacles)
+        {
+            // Retrieve the visual representation of the obstacle (e.g., sprite or texture)
+            SpriteRenderer obstacleRenderer = obstacle.GetComponent<SpriteRenderer>();
+            Texture2D obstacleTexture = obstacleRenderer.sprite.texture;
+
+            // Determine the position on the texture
+            Vector2 texturePosition = CalculateTexturePosition(obstacle.transform.position, textureWidth, textureHeight);
+
+            // Determine the scaling factors for the obstacle
+            float scaleX = obstacle.transform.localScale.x * (float)obstacleTexture.width / Screen.width;
+            float scaleY = obstacle.transform.localScale.y * (float)obstacleTexture.height / Screen.height;
+
+            texturePosition += new Vector2((textureWidth / 2) - (obstacleTexture.width * scaleX/2), (textureHeight / 2) - (obstacleTexture.height * scaleY/2));
+
+            // Iterate over the pixels of the obstacle's texture
+            for (int x = 0; x < obstacleTexture.width; x++)
+            {
+                for (int y = 0; y < obstacleTexture.height; y++)
+                {
+                    // Calculate the texture coordinate
+                    int textureX = (int)(texturePosition.x + x * scaleX);
+                    int textureY = (int)(texturePosition.y + y * scaleY);
+
+                    // Check if the texture coordinate is within the texture bounds
+                    if (IsWithinTextureBounds(new Vector2Int(textureX, textureY), canvasTexture))
+                    {
+                        // Retrieve the pixel color from the obstacle's texture
+                        Color obstaclePixel = obstacleTexture.GetPixel(x, y);
+
+                        // Set the corresponding pixel on the main texture
+                        canvasTexture.SetPixel(textureX, textureY, obstaclePixel);
+                    }
+                }
+            }
+        }
+
+    // Apply the changes to the texture
+    canvasTexture.Apply();
+    }
+
+    private Vector2Int CalculateTexturePosition(Vector3 worldPosition, int textureWidth, int textureHeight)
+    {
+        // Convert the world position to texture coordinates
+        float normalizedX = (worldPosition.x / Screen.width) * textureWidth;
+        float normalizedY = (worldPosition.y / Screen.height) * textureHeight;
+
+        // Convert the normalized coordinates to integer texture coordinates
+        int x = Mathf.RoundToInt(normalizedX);
+        int y = Mathf.RoundToInt(normalizedY);
+
+        // Clamp the coordinates to ensure they are within the texture bounds
+        x = Mathf.Clamp(x, 0, textureWidth - 1);
+        y = Mathf.Clamp(y, 0, textureHeight - 1);
+
+        return new Vector2Int(x, y);
+    }
+
+    private bool IsWithinTextureBounds(Vector2Int textureCoordinate, Texture2D texture)
+    {
+        int x = textureCoordinate.x;
+        int y = textureCoordinate.y;
+
+        // Check if the coordinates are within the texture bounds
+        return x >= 0 && x < texture.width && y >= 0 && y < texture.height;
     }
 
     private void Update()
